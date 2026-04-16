890159966bfa06fd6b81c9cd520ad835e4bcf61e
Julio Lira <yurgenlira@hotmail.com>
2026-04-16 14:57:37 -0600

    fix(iam): allow OIDC from pull_request events

--- infra/terraform/modules/iam_github_oidc/main.tf
+++ infra/terraform/modules/iam_github_oidc/main.tf
@@ -25,7 +25,10 @@ resource "aws_iam_role" "github_actions" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:ref:refs/heads/main"
+          "token.actions.githubusercontent.com:sub" = [
+            "repo:${var.github_repo}:ref:refs/heads/main",
+            "repo:${var.github_repo}:pull_request"
+          ]
         }
       }
     }]
